fd9d5ee8c523e8034882824716409815029de6d9
Dillon Caldwell <dcaldwell1228@gmail.com>
2024-10-20 01:00:31 -0400

    change file names

--- run_scripts.sh
+++ run_scripts.sh
@@ -2,18 +2,18 @@
 
 # Run the script to create team directories
 echo "Preprocesssing the images..."
-python3 preprocess_images.py
+python3 01_preprocess_images.py
 
 # Run the script to 
 echo "Extract text from images..."
-python3 extract_text_from_image.py
+python3 02_extract_text_from_image.py
 
 # Run the script to create team directories
 echo "Creating team directories..."
-python3 create_team_dirs.py
+python3 03_create_team_dirs.py
 
 # Run the script to move images to team directories
 echo "Moving images to team directories..."
-python3 mv_images_to_teams.py
+python3 04_mv_images_to_teams.py
 
 echo "Done."
